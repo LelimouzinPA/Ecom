6f5a719414ba264a9968c3233a78937d3dc0781d
MathiasCabrol <mathiascabrol@gmail.com>
2021-11-22 22:36:32 +0100

    Finalisation du javascript

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,27 +1,32 @@
 //Appel du JSON selon les catégories
 let uri = "categorie1.json"
+categoryTitle.innerText = "Nourriture"
 foodBtn.addEventListener("click", function() {
     uri = "categorie1.json"
-    searchMovie()
+    searchCategory()
+    categoryTitle.innerText = "Nourriture"
 })
 hygienBtn.addEventListener("click", function() {
     uri = "categorie2.json"
-    searchMovie()
+    searchCategory()
+    categoryTitle.innerText = "Produits d'hygiéne"
 })
 catgameBtn.addEventListener("click", function() {
     uri = "categorie3.json"
-    searchMovie()
+    searchCategory()
+    categoryTitle.innerText = "Jouets"
 })
 MaterielCat.addEventListener("click", function() {
         uri = "categorie4.json"
-        searchMovie()
+        searchCategory()
+        categoryTitle.innerText = "Matériel"
     })
     //Fonction Json
-const searchMovie = () => {
+const searchCategory = () => {
         fetch("http://127.0.0.1:5500/assets/json/" + uri)
             .then(fetchResponse => fetchResponse.json())
             .then(jsonContent => {
-                clearMovie()
+                clearCategory()
                 jsonContent.results.map((article, index) => {
                     //Création de la div principale
                     let coldiv = document.createElement("div")
@@ -46,7 +51,7 @@ const searchMovie = () => {
                     cardptitle.innerText = (article.description != "") ? article.description.substring(0, 200) + " ..." : null
                         //Overlay masque au-dessus de la card
                     let pricepdiv = document.createElement("p")
-                    pricepdiv.innerText = article.price
+                    pricepdiv.innerText = article.price + " €"
                     pricepdiv.className = "badge bg-success text-wrap col-6"
                         //Div row des boutons
                     let innerCardRow = document.createElement("div")
@@ -88,7 +93,12 @@ const searchMovie = () => {
                     confirmButton.innerText = "Valider"
                     confirmButton.id = "confirm" + article.id
                     innerCardRow.appendChild(confirmButton)
+                    let totalPriceNumber = 0
                     confirmButton.addEventListener("click", cartProduct = () => {
+                        //Création du rowCart
+                        let rowCart = document.createElement("div")
+                        rowCart.className = "row"
+                        containerModal.appendChild(rowCart)
                             //Div première col
                             let productModalDiv1 = document.createElement("div")
                             productModalDiv1.className = "col-3"
@@ -97,63 +107,91 @@ const searchMovie = () => {
                                 // creation de img 
                             let productImage = document.createElement("img")
                             productImage.src = article.image
-                            productImage.className = "col-3"
+                            productImage.className = "col-3 img-fluid"
                             productModalDiv1.appendChild(productImage)
                                 // creation de la seconde div col 3
                             let productModalDiv2 = document.createElement("div")
-                            productModalDiv2.className = "col-4"
+                            productModalDiv2.className = "col-2"
                             rowCart.appendChild(productModalDiv2)
                                 // creation du paragraphe titre
                             let pProductTitle = document.createElement("p")
                             pProductTitle.innerText = article.product_name
                             productModalDiv2.appendChild(pProductTitle)
-                                //let clonedProduct = coldiv.cloneNode(true)
-                                //rowCart.append(clonedProduct)
+                            //Création de la div tarif
+                            let cartPriceDiv = document.createElement("div")
+                            cartPriceDiv.className = "col-2"
+                            rowCart.appendChild(cartPriceDiv)
+                                //Création de l'input du nombre de produits
                             let innerDivModal = document.createElement("div")
                             innerDivModal.className = "col-2"
                             let innerCardRowModal = document.createElement("div")
-                            innerCardRowModal.className = "  mt-3 justify-content-center pb-3"
+                            innerCardRowModal.className = "justify-content-center"
                                 //Création du bouton moins
                             const minusButtonModal = document.createElement("a")
-                            minusButtonModal.className = "col-1 btn btn-danger"
+                            minusButtonModal.className = "col-4 btn btn-danger"
                             minusButtonModal.innerText = "-"
                             minusButtonModal.id = "minusModal" + article.id
                             innerDivModal.appendChild(minusButtonModal)
                                 //Fonction diminution quantité au clic
                             minusButtonModal.addEventListener("click", removeProductModal = () => {
                                     if (productsInputModal.value > 0) {
-                                        productsInputModal.value--
+                                    productsInputModal.value--
+                                    articlePrice = article.price
+                                    result = articlePrice * productsInputModal.value
+                                    paraPrice.innerText = result.toFixed(2)
+                                    totalPriceNumber = totalPriceNumber - article.price
+                                    modalTotalPrice.innerText = "Montant total : " + totalPriceNumber.toFixed(2) + " €"
                                     } else {
                                         productsInputModal.value == 0
                                     }
                                 })
-                                //Création de l'input du nombre de produits
+                                //Partie du code dans le modal
                             var productsInputModal = document.createElement("input")
                             productsInputModal.setAttribute("type", "text")
-                            productsInputModal.className = "col-2"
-                            productsInputModal.value = 0
+                            productsInputModal.className = "col-4"
+                            productsInputModal.value = productsInput.value
                             productsInputModal.id = "inputModal" + article.id
                             innerDivModal.appendChild(productsInputModal)
+                            //Création prix
+                            var paraPrice = document.createElement("p")
+                            //Calcul du prix
+                            articlePrice = article.price
+                            result = articlePrice * productsInput.value
+                            paraPrice.innerText = result.toFixed(2) + " €"
+                            cartPriceDiv.appendChild(paraPrice)
                                 //Création du bouton plus
                             const plusButtonModal = document.createElement("a")
-                            plusButtonModal.className = "col-1 btn btn-success"
+                            plusButtonModal.className = "col-4 btn btn-success"
                             plusButtonModal.innerText = "+"
                             plusButtonModal.id = "plusModal" + article.id
                             innerDivModal.appendChild(plusButtonModal)
                                 //Fonction ajout quantité au clic
                             plusButtonModal.addEventListener("click", addProductModal = () => {
                                 productsInputModal.value++
+                                articlePrice = article.price
+                                result = articlePrice * productsInputModal.value
+                                paraPrice.innerText = result.toFixed(2)
+                                totalPriceNumber = totalPriceNumber + article.price
+                                modalTotalPrice.innerText = "Montant total : " + totalPriceNumber.toFixed(2) + " €"
                             })
                             rowCart.appendChild(innerDivModal)
                             let valDivModal = document.createElement("div")
                             valDivModal.className = "col-2"
                                 //Création du bouton valider
-                            const confirmButtonModal = document.createElement("a")
-                            confirmButtonModal.className = "btn btn-primary mt-3"
-                            confirmButtonModal.innerText = "Valider"
-                            confirmButtonModal.id = "confirm" + article.id
-                            valDivModal.appendChild(confirmButtonModal)
+                            const deleteButtonModal = document.createElement("a")
+                            deleteButtonModal.className = "btn btn-warning"
+                            deleteButtonModal.innerText = "Supprimer"
+                            deleteButtonModal.id = "deleteModal" + article.id
+                            deleteButtonModal.addEventListener("click", deleteRowCart = () => {
+                                totalPriceNumber = totalPriceNumber - result
+                                modalTotalPrice.innerText = "Montant total : " + totalPriceNumber.toFixed(2) + " €"
+                                rowCart.remove()
+                            })
+                            valDivModal.appendChild(deleteButtonModal)
                             rowCart.append(valDivModal)
+                            //Création du paragraphe affichant le résultat total
+                            totalPriceNumber = totalPriceNumber + result
+                            modalTotalPrice.innerText = "Montant total : " + totalPriceNumber.toFixed(2) + " €"
                         })
                         //Accrochage des divs
                     carddiv.append(cardtitle, cardptitle)
@@ -164,13 +202,8 @@ const searchMovie = () => {
             })
     }
     //Suppression du contenu précédent
-const clearMovie = () => {
+const clearCategory = () => {
     articleContent.innerText = ""
 }
-searchMovie()
-    //fonction valeur de l'input pour afficher 
-    //div quantité 
-    //div prix
-    //boutton + input - supprimer
-    //montant total addition tarif prix total + bouton acheter 
+searchCategory()
     //bonus panier jolie
